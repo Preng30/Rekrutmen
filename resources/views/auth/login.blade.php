0f05556ca7d54590e12d122385791d5093e81c26
adiakpreng <prengki19si@mahasiswa.pcr.ac.id>
2024-01-09 09:07:01 +0700

    commit ketiga

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -29,8 +29,12 @@
                             <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
+                                <div class="input-group">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
+                                <button class="btn btn-outline-secondary toggle-password" type="button">
+                                    <i class="fa fa-eye"></i>
+                                </button>
+                                </div>
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -57,11 +61,11 @@
                                     {{ __('Login') }}
                                 </button>
 
-                                @if (Route::has('password.request'))
+                                {{-- @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
                                         {{ __('Forgot Your Password?') }}
                                     </a>
-                                @endif
+                                @endif --}}
                             </div>
                         </div>
                     </form>
@@ -71,3 +75,49 @@
     </div>
 </div>
 @endsection
+<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<script>
+    $(document).ready(function () {
+        // Show/hide password
+        $('.toggle-password').on('click', function () {
+            var passwordInput = $(this).prev('input');
+            var type = passwordInput.attr('type') === 'password' ? 'text' : 'password';
+            passwordInput.attr('type', type);
+        });
+
+        // Login form submission with SweetAlert2
+        $('form').submit(function (e) {
+            e.preventDefault(); 
+
+            var form = $(this);
+
+            $.ajax({
+                type: form.attr('method'),
+                url: form.attr('action'),
+                data: form.serialize(),
+                success: function (response) {
+                    Swal.fire({
+                        title: 'Login Berhasil',
+                        icon: 'success',
+                        showConfirmButton: false,
+                        timer: 1500
+                    }).then(function () {
+                        window.location.href = '/home';
+                    });
+                },
+                error: function (xhr, status, error) {
+                    Swal.fire({
+                        title: 'Login Gagal',
+                        text: 'Email atau password salah',
+                        icon: 'error',
+                        showConfirmButton: false,
+                        timer: 2000
+                    });
+                }
+            });
+        });
+    });
+</script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-... (integrity value) ..." crossorigin="anonymous" />
+
